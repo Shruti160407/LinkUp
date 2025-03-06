90f1f0e6350f8b06b805d3792176e1fe3ac83ad6
Shruti160407 <shrutichourasia1607@gmail.com>
2025-03-07 01:25:37 +0530

    new changes

--- linkup/src/app/page.tsx
+++ linkup/src/app/page.tsx
@@ -1,10 +1,11 @@
+import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
               <SignInButton mode="modal">
               <Button>
@@ -16,6 +17,7 @@ export default function Home() {
               <UserButton />
             </SignedIn>
 
+<ModeToggle />
             <Button variant={"secondary"}>Click me</Button>
     </div>
   );
