a5a5607dd5f009015de1b8e55ee73bf1b448bbfd
Shruti160407 <shrutichourasia1607@gmail.com>
2025-03-07 00:59:00 +0530

    new dependencies installed

--- linkup/src/app/page.tsx
+++ linkup/src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello home page</h1>
+      <SignedOut>
+              <SignInButton mode="modal">
+              <Button>
+                Sign in
+              </Button>
+                </SignInButton>
+              </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
