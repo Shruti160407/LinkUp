f27a15d89f5fcc976fbd1c27f1cef823cd0156bf
Shruti160407 <shrutichourasia1607@gmail.com>
2025-04-14 22:13:19 +0530

    navbar, sidebar & middleware updated

--- linkup/components/Navbar.tsx
+++ linkup/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+/*import Link from 'next/link'
 import React from 'react'
 import DesktopNavbar from './DesktopNavbar'
 import MobileNavbar from './MobileNavbar'
@@ -27,4 +27,37 @@ async function Navbar() {
   )
 }
 
+export default Navbar*/
+
+
+// Example of server component
+import { currentUser } from '@clerk/nextjs/server'
+import DesktopNavbar from './DesktopNavbar'
+import MobileNavbar from './MobileNavbar'
+import { syncUser } from '@/src/actions/user.action'
+import Link from 'next/link';
+
+async function Navbar() {
+  const user = await currentUser();
+  if(user) await syncUser(); //POSTS
+
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
+              LinkUp
+            </Link>
+          </div>
+
+          <DesktopNavbar />
+          <MobileNavbar />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
 export default Navbar
+
