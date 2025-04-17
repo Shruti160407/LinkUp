d3b6e8dbac3598a74ecc79b9db602eefce813c0c
Shruti160407 <shrutichourasia1607@gmail.com>
2025-04-17 23:21:35 +0530

    post creation

--- linkup/src/app/layout.tsx
+++ linkup/src/app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+});  
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
